e30726adc40979635bd827ee8446542fee5d89e8
goartisan <you@example.com>
2020-12-23 20:46:03 +0900

    move query of profile tweets to Tweet model

--- server/app/Http/Controllers/ProfileController.php
+++ server/app/Http/Controllers/ProfileController.php
@@ -125,36 +125,9 @@ class ProfileController extends Controller
     {
         $authId = $request->session()->get('auth') ? $request->session()->get('auth')->id : 0;
         $profile = User::where('username', $username)->first();
-        $select = ['tweets.*', 'u.avatar', 'u.fullname', 'u.username'];
-        $tweets = Tweet::select($select)
-            ->selectRaw('count(distinct l_a.id) as num_likes')
-            ->selectRaw('case when l_b.user_id = ' . $authId . ' then 1 else 0 end as is_liked')
-            ->selectRaw('count(distinct r_a.id) as num_retweets')
-            ->selectRaw('case when r_b.user_id = ' . $authId . ' then 1 else 0 end as is_retweeted')
-            ->join('users as u', function ($join) use ($profile) {
-                $join->on('tweets.user_id', '=', 'u.id')
-                    ->whereNull('u.deleted_at')
-                    ->where('tweets.user_id', $profile->id);
-            })
-            ->leftJoin('likes as l_a', function ($join) {
-                $join->on('tweets.id', '=', 'l_a.tweet_id')
-                    ->whereNull('l_a.deleted_at');
-            })->groupBy('tweets.id')
-            ->leftJoin('likes as l_b', function ($join) use ($authId) {
-                $join->on('tweets.id', '=', 'l_b.tweet_id')
-                    ->whereNull('l_b.deleted_at')
-                    ->where('l_b.user_id', $authId);
-            })->groupBy('l_b.id')
-            ->leftJoin('retweets as r_a', function ($join) {
-                $join->on('tweets.id', '=', 'r_a.tweet_id')
-                    ->whereNull('r_a.deleted_at');
-            })->groupBy('tweets.id')
-            ->leftJoin('retweets as r_b', function ($join) use ($authId) {
-                $join->on('tweets.id', '=', 'r_b.tweet_id')
-                    ->whereNull('r_b.deleted_at')
-                    ->where('r_b.user_id', $authId);
-            })->groupBy('r_b.id')
-            ->orderBy('tweets.created_at', 'desc')->get();
+
+        // get tweets by profile id
+        $tweets = Tweet::getTweetsByUserId($profile->id, $authId);
 
         return view('profile.profile', ['profile' => $profile, 'tweets' => $tweets]);
     }
--- server/app/Models/Tweet.php
+++ server/app/Models/Tweet.php
@@ -19,4 +19,43 @@ class Tweet extends Model
     protected $attributes = [
         'text' => '',
     ];
+
+    public static function getTweetsByUserId($userId, $authId)
+    {
+        $select = [
+            'tweets.*', 'tweets.created_at as time',
+            'u.avatar', 'u.fullname', 'u.username'
+        ];
+        $tweets = Tweet::select($select)
+            ->selectRaw('count(distinct l_a.id) as num_likes')
+            ->selectRaw('case when l_b.user_id = ' . $authId . ' then 1 else 0 end as is_liked')
+            ->selectRaw('count(distinct r_a.id) as num_retweets')
+            ->selectRaw('case when r_b.user_id = ' . $authId . ' then 1 else 0 end as is_retweeted')
+            ->join('users as u', function ($join) use ($userId) {
+                $join->on('tweets.user_id', '=', 'u.id')
+                    ->whereNull('u.deleted_at')
+                    ->where('tweets.user_id', $userId);
+            })
+            ->leftJoin('likes as l_a', function ($join) {
+                $join->on('tweets.id', '=', 'l_a.tweet_id')
+                    ->whereNull('l_a.deleted_at');
+            })->groupBy('tweets.id')
+            ->leftJoin('likes as l_b', function ($join) use ($authId) {
+                $join->on('tweets.id', '=', 'l_b.tweet_id')
+                    ->whereNull('l_b.deleted_at')
+                    ->where('l_b.user_id', $authId);
+            })->groupBy('l_b.id')
+            ->leftJoin('retweets as r_a', function ($join) {
+                $join->on('tweets.id', '=', 'r_a.tweet_id')
+                    ->whereNull('r_a.deleted_at');
+            })->groupBy('tweets.id')
+            ->leftJoin('retweets as r_b', function ($join) use ($authId) {
+                $join->on('tweets.id', '=', 'r_b.tweet_id')
+                    ->whereNull('r_b.deleted_at')
+                    ->where('r_b.user_id', $authId);
+            })->groupBy('r_b.id')
+            ->orderBy('tweets.created_at', 'desc')->get();
+
+        return $tweets;
+    }
 }

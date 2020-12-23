224f2eeaf1796819f98df16412051805a638bfa5
goartisan <you@example.com>
2020-12-23 19:31:10 +0900

    add retweets table

---
+++ server/app/Models/Retweet.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Retweet extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'retweets';
+
+    protected $fillable = [
+        'user_id', 'tweet_id',
+    ];
+}

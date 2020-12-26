81374dcd97ce71d53d22623555154c883b815d71
goartisan <you@example.com>
2020-12-26 09:18:50 +0900

    add pagination.blade.php

---
+++ server/resources/views/layouts/pagination.blade.php
@@ -0,0 +1,27 @@
+<?php $p = $pagination; ?>
+<div style="text-align: right">
+    <ul class="pagination">
+        <!-- previous button -->
+        <li class="page-item {{ $p->current_page <= 1 ? 'disabled' : '' }}">
+            @if ($p->current_page <= 1)
+                <span class="page-link">Previous</span>
+            @else
+                <a class="page-link" href="{{ $page_link . '?page=' . ($p->current_page - 1) }}">Previous</a>
+            @endif
+        </li>
+        <!-- page items -->
+        @for ($i = 1; $i <= ($p->total / $p->per_page) + 1; $i++)
+            <li class="page-item {{ $p->current_page == $i ? 'active' : '' }}">
+                <a class="page-link" href="{{ $page_link . '?page=' . $i }}">{{ $i }}</a>
+            </li>
+        @endfor
+        <!-- next button -->
+        <li class="page-item {{ $p->current_page >= ($p->total / $p->per_page) ? 'disabled' : '' }}">
+            @if ($p->current_page >= ($p->total / $p->per_page))
+                <span class="page-link">Next</span>
+            @else
+                <a class="page-link" href="{{ $page_link . '?page=' . ($p->current_page + 1) }}">Next</a>
+            @endif
+        </li>
+    </ul>
+</div>

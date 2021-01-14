e73fe639da54ca660e0c9ac5093349b19ddc13c6
goartisan <you@example.com>
2021-01-14 19:40:35 +0900

    add replies and pins to related deletings

--- server/app/Models/Tweet.php
+++ server/app/Models/Tweet.php
@@ -6,14 +6,24 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
-use App\Models\Retweet;
 use App\Models\Like;
+use App\Models\Retweet;
+use App\Models\Reply;
+use App\Models\Pin;
 
 class Tweet extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $table = 'tweets';
+
+    /**
+     * get a query to retrieve the likes for the tweet
+     */
+    public function likes()
+    {
+        return $this->hasMany(Like::class, 'tweet_id', 'id');
+    }
 
     /**
      * get a query to retrieve the retweets for the tweet
@@ -24,11 +34,19 @@ class Tweet extends Model
     }
 
     /**
-     * get a query to retrieve the likes for the tweet
+     * get a query to retrieve the replies for the tweet
      */
-    public function likes()
+    public function replies()
     {
-        return $this->hasMany(Like::class, 'tweet_id', 'id');
+        return $this->hasMany(Reply::class, 'reply_id', 'id');
+    }
+
+    /**
+     * get a query to retrieve the pins for the tweet
+     */
+    public function pins()
+    {
+        return $this->hasMany(Pin::class, 'tweet_id', 'id');
     }
 
     /**
@@ -40,8 +58,10 @@ class Tweet extends Model
 
         static::deleting(function ($tweet) {
             DB::transaction(function () use ($tweet) {
-                $tweet->retweets()->delete();
                 $tweet->likes()->delete();
+                $tweet->retweets()->delete();
+                $tweet->replies()->delete();
+                $tweet->pins()->delete();
             });
         });
     }

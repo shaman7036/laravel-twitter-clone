eb9d5737238cc01b35d1ba7671df54bcb7db3298
goartisan <you@example.com>
2021-01-19 02:30:36 +0900

    fix timezone at date in tweet

--- server/resources/views/layouts/variables.blade.php
+++ server/resources/views/layouts/variables.blade.php
@@ -1,4 +1,8 @@
 <script>
+// auth data
 let auth = '';
 <?php if(Session::get('auth')) echo 'auth = ' . Session::get('auth'); ?>;
+
+// timezone offset
+const timezoneOffset = -(new Date().getTimezoneOffset());
 </script>
